9b10c97f4b75faff8e3afd3da2aa0864d63335e2
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-11-06 15:40:54 -0800

    documented and rewrote methods

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnBottomMiddle: Button
     private lateinit var btnBottomRight: Button
 
+    // A 2 dimensional list so that it's easier to find wins
     private lateinit var buttons: List<List<Button>>
 
     private lateinit var tvPlayerInfo: TextView
@@ -77,6 +78,7 @@ class MainActivity : AppCompatActivity() {
                 button.isEnabled = true // Ensure the button is enabled
             }
         }
+        // Make it player x's turn
         xTurn = true
     }
 
@@ -86,6 +88,7 @@ class MainActivity : AppCompatActivity() {
         // disable the button so the user can no longer select it
         button.isEnabled = false
 
+        // change the text of the button based on who's turn it is
         if (xTurn) {
             button.text = "X"
             tvPlayerInfo.text = getString(R.string.player_O_s_turn)
@@ -95,14 +98,19 @@ class MainActivity : AppCompatActivity() {
             button.text = "O"
         }
 
+        // check for a win
         isWin()
 
+        // check for a tie
         isTie()
 
         // set xTurn to the opposite of what it currently is
         xTurn = !xTurn
     }
 
+    // A method to check if there is a win on the board.
+    // If there is, it will display the results,
+    // and lock the play buttons
     private fun isWin(){
         if (isHorizontalWin() || isVerticalWin() || isDiagonalWin()) {
             tvPlayerInfo.text = if (xTurn) "Player X wins!" else "Player O Wins!"
@@ -110,57 +118,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Checks if there is a win in a horizontal row
     private fun isHorizontalWin() : Boolean {
-        var win = true
-
         for (i in 0..2) {
-            win = if(xTurn) {
-                buttons[i][0].text.equals("X")
-                        && buttons[i][1].text.equals("X")
-                        && buttons[i][2].text.equals("X")
-            } else {
-                buttons[i][0].text.equals("O")
-                        && buttons[i][1].text.equals("O")
-                        && buttons[i][2].text.equals("O")
-            }
-            if (win) return true
+            // returns true if a row contains a win (all text values are the same)
+            if (buttons[i][0].text.equals(buttons[i][1].text)
+            && buttons[i][2].text.equals(buttons[i][0].text)) return true
         }
-        return win
+        // returns false if no row contains a win
+        return false
     }
 
+    // Checks for a win in vertical columns
     private fun isVerticalWin() : Boolean {
-        var win = false
-
         for (i in 0..2) {
-            win = if(xTurn) {
-                buttons[0][i].text.equals("X")
-                        && buttons[1][i].text.equals("X")
-                        && buttons[2][i].text.equals("X")
-            }
-            else {
-                buttons[0][i].text.equals("O")
-                        && buttons[1][i].text.equals("O")
-                        && buttons[2][i].text.equals("O")
-            }
-            if (win) return true
+            // returns true if there is a win in the columns
+            if(buttons[0][i].text.equals(buttons[1][i].text)
+                && buttons[2][i].text.equals(buttons[0][i].text)) return true
         }
-
-        return win
+        // returns false if no column contains a win
+        return false
     }
 
+    // checks each diagonal for a win
     private fun isDiagonalWin(): Boolean {
+        // returns true if there is a win from the top left
+        // to the bottom right
         return if (btnTopLeft.text.equals(btnMiddle.text)
             && btnMiddle.text.equals(btnBottomRight.text)
             && !btnMiddle.text.isNullOrBlank()) true
+        // returns true if there is a win from the top right
+        // to the bottom left
         else if (btnTopRight.text.equals(btnMiddle.text)
             && btnMiddle.text.equals(btnBottomLeft.text)
             && !btnMiddle.text.isNullOrBlank()) true
+        // otherwise returns false
         else false
     }
 
+    // Checks for a tie, and if there is a tie
+    // displays the tie message
     private fun isTie(){
+        // checks for button that hasn't been clicked on yet
         for (list in buttons) {
             for (button in list) {
+                // if a button hasn't been clicked on, return out of the loop
                 if (button.text.isNullOrBlank()) return
             }
         }
@@ -169,6 +171,7 @@ class MainActivity : AppCompatActivity() {
         disablePlayButtons()
     }
 
+    // disables all 9 play buttons on the screen
     private fun disablePlayButtons() {
         for (list in buttons) {
             for (button in list) {
